34ab3a150e217e40bd991241c65bf10578ffb61c
l2t <l2t@gmail.com>
2026-01-25 14:36:19 +0530

    Added stream examples

--- learning_core_java/src/main/java/com/l2t/lcj/collections/streams/Player.java
+++ learning_core_java/src/main/java/com/l2t/lcj/collections/streams/Player.java
@@ -4,12 +4,10 @@ import lombok.Data;
 
 @Data
 public class Player {
-    private int id;
+
     private String name;
     private String country;
     private String team;
     private String role;
-    private int age;
-    private int rating;
     private int amount;
 }
