675b2ee1a4c9df3560f047b45f1d04db5cde3711
l2t <l2t@gmail.com>
2026-02-19 19:59:09 +0530

    Added docker file to build and deploy

---
+++ cbook/src/main/java/com/l2t/cbook/CbookBeAppApplication.java
@@ -0,0 +1,13 @@
+package com.l2t.cbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbookBeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbookBeAppApplication.class, args);
+	}
+
+}
